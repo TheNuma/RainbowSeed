edc56c08b1e5aa32786e57e120461d9c0b05cd1b
DESKTOP-UPOJ9CR\Numa <ant_numa@hotmail.com>
2026-05-01 13:36:53 -0400

    Items now delete based on sets!

--- core/src/main/java/com/numa/rainbow/RainbowSeedGame.java
+++ core/src/main/java/com/numa/rainbow/RainbowSeedGame.java
@@ -1,5 +1,7 @@
 package com.numa.rainbow;
 
+import java.util.HashSet;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -16,20 +18,25 @@ public class RainbowSeedGame extends ApplicationAdapter {
 	
 	private Stage stage;
     ItemInteractions interactions;
+    HashSet<ItemType> dirtSet =new HashSet<>();
+    HashSet<ItemType> waterSet=new HashSet<>();
+    HashSet<ItemType> grassSet=new HashSet<>();
 
     @Override
     public void create() {
         initializeUI();
-        Item dirt1 = new Item(ItemType.DIRT);
-        Item dirt2 = new Item(ItemType.DIRT);
-        Item water = new Item(ItemType.WATER);
+ 
+        dirtSet.add(ItemType.GRASS);
+        Item dirt1 = new Item(ItemType.DIRT, dirtSet);
+//        Item dirt2 = new Item(ItemType.DIRT);
+        Item water = new Item(ItemType.WATER,waterSet);
         stage.addActor(dirt1);
-        stage.addActor(dirt2);
+//        stage.addActor(dirt2);
         stage.addActor(water);
         interactions = new ItemInteractions();
         combineItems(dirt1, water);
-        combineItems(dirt1,dirt2);
-        combineItems(water,dirt2);
+//        combineItems(dirt1,dirt2);
+//        combineItems(water,dirt2);
     }
 
 	@Override
@@ -53,11 +60,16 @@ public class RainbowSeedGame extends ApplicationAdapter {
     public void combineItems(Item item1, Item item2) {
        	if (interactions.hasCombinations(item1.getType(), item2.getType())) {
     		//create new object
-    		ItemType type3 = interactions.getCombination(item1.getType(), item2.getType());    		
+    		ItemType type3 = interactions.getCombination(item1.getType(), item2.getType());    
+    		
+    		grassSet.add(ItemType.WHEAT);
+    		
     		System.out.println(item1.getType()+" and "+item2.getType()+ " combined to make "+type3);
-    		Item spawnItem =new Item(type3);
+    		Item spawnItem =new Item(type3,grassSet);
     		stage.addActor(spawnItem);
     		spawnItem.setPosition((item1.getX()+item2.getX())/2f, (item1.getY()+item2.getY())/2f);
+    		item1.removeCombo(type3);
+    		item2.removeCombo(type3);
     		//remove 'parent' objects if necessary
     		checkAndRemoveSingleUse(item1);
     		checkAndRemoveSingleUse(item2);
@@ -68,7 +80,7 @@ public class RainbowSeedGame extends ApplicationAdapter {
     }
     
     public void checkAndRemoveSingleUse(Item t) {
-    	if (t.getType().isSingleUse())
+    	if (t.getType().isSingleUse()&&!t.hasRemainingCombinations())
     	{
     		System.out.println("All combinations with "+t.getType().toString()+" has been found");
     		t.remove();
--- core/src/main/java/com/numa/rainbow/items/Item.java
+++ core/src/main/java/com/numa/rainbow/items/Item.java
@@ -1,21 +1,36 @@
 package com.numa.rainbow.items;
 
+import java.util.HashSet;
 import java.util.Random;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class Item extends Actor{
+	private HashSet<ItemType> remainingCombinations;
+
 	private ItemType type;
+	private boolean hasRemainingCombinations;
 	
-	public Item(ItemType type) {
+	public Item(ItemType type,HashSet combos) {
 		this.type=type;
+		this.remainingCombinations=combos;
+		this.hasRemainingCombinations=true;
 		this.setSize(40, 40);
 		this.setPosition(new Random().nextInt(500),new Random().nextInt(500));
 		this.debug();
 	}
 	
+	public void removeCombo(ItemType type) {
+		remainingCombinations.remove(type);
+		if(remainingCombinations.isEmpty()) {
+			hasRemainingCombinations= false;
+		}
+	}
+	
 	public ItemType getType() {
 		return type;
 	}
-	
+	public boolean hasRemainingCombinations() {
+		return hasRemainingCombinations;
+	}
 }
