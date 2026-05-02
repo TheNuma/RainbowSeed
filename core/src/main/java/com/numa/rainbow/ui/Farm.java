75320c6d97febdc44815226990033b9ba675dbd4
DESKTOP-UPOJ9CR\Numa <ant_numa@hotmail.com>
2026-05-02 09:19:36 -0400

    Making creating object cleaner

--- core/src/main/java/com/numa/rainbow/ui/Farm.java
+++ core/src/main/java/com/numa/rainbow/ui/Farm.java
@@ -1,5 +1,8 @@
 package com.numa.rainbow.ui;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.numa.rainbow.items.Combiner;
 import com.numa.rainbow.items.DraggableItem;
@@ -7,25 +10,30 @@ import com.numa.rainbow.items.ItemInteractions;
 import com.numa.rainbow.items.ItemType;
 
 public class Farm {
+	private final Map<ItemType,DraggableItem> allItems;
 	private final ItemInteractions interactions;
+	private final Stage stage;
 	public Farm(Stage stage) {
+		this.stage=stage;
+		allItems = new HashMap<>();
 		interactions =new ItemInteractions();
 		UI.setItemInteractions(interactions);
 		Combiner.setItemInteractions(interactions);
 		
-		DraggableItem seeds = UI.makeDraggableItem(ItemType.SEED);
-		seeds.setPosition(740, 320);
-		stage.addActor(seeds);
-
-		DraggableItem wateringCan = UI.makeDraggableItem(ItemType.WATER);
-		wateringCan.setPosition(500, 500);
-		stage.addActor(wateringCan);
-
-		DraggableItem dirt = UI.makeDraggableItem(ItemType.DIRT);
-		dirt.setPosition(999, 505);
-		stage.addActor(dirt);
+		makeItem(ItemType.SEED,740,320);
+		makeItem(ItemType.WATER, 500, 500);
+		makeItem(ItemType.DIRT,999,505);
 		
-		setUpItemPair(wateringCan, dirt);
+		setUpItemPair(allItems.get(ItemType.WATER), allItems.get(ItemType.DIRT));
+	}
+	private void makeItem(ItemType itemType,float x, float y) {
+		DraggableItem item = UI.makeDraggableItem(itemType);
+		allItems.put(itemType, item);
+		item.setPosition(x,y);
+		stage.addActor(item);
+	}
+	private void setupAllItems() {
+		
 	}
 	
 	private void setUpItemPair(DraggableItem item1, DraggableItem item2) {
