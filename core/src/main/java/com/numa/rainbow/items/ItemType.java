b68d3bb308708c9d52449ca9a3f56c5067a49f56
DESKTOP-UPOJ9CR\Numa <ant_numa@hotmail.com>
2026-05-02 11:39:50 -0400

    all items now loaded in farm

--- core/src/main/java/com/numa/rainbow/items/ItemType.java
+++ core/src/main/java/com/numa/rainbow/items/ItemType.java
@@ -1,16 +1,45 @@
 package com.numa.rainbow.items;
 
 public enum ItemType {
-	SEED("sesame","Seed"),
+	SUMMERSEED("sesame","Seeds"),
 	DIRT("dirt","Dirt"),
+//	WEED(,"Weeds"),
 	GRASS("grass","Grass"),
 	WATER("watering-can","Water"),
-	SEAWATER("wave-crest","Sea Water");
-//	SUN(false),
-//	SEAWEED(true),
-//	DANDELION(true),
-//	WHEAT(true),
-//	REED(false);
+	ROCK("rock","Rock"),
+	SUN("sun","Sun"),	
+	SEAWATER("wave-crest","Sea Water"),
+	STRAWBERRY("strawberry","Strawberry"),
+	FALLSEED("seasonal-seed","Seeds"),
+	SPRINGSEED("ground-sprout","Seeds"),
+	BAMBOO("bamboo","Bamboo"),
+//	BUSH(,"Bush"),
+	WHEAT("wheat","Wheat"),
+	SCARECROW("scarecrow","Scarecrow"),
+	TREE("tree","Tree"),
+	AXE("axe","Axe"),
+	STICK("stick","Stick"),
+//	TULIP(,"Tulip"),
+	BERRYBUSH("berry-bush","Berry Bush"),
+	VINE("vines","Vines"),
+	BRAMBLES("brambles","Brambles"),
+	PUMPKIN("pumpkin","Pumpkins"),
+	SNOWDROP("snowdrop","Snowdrop"),
+	DANDELION("dandelion","Dandelion"),
+	REED("reed","Reed"),
+	FLAX("flax","Flax"),
+	HOPS("hops","Hops"),	
+	SEAWEED("seaweed","Seaweed"),
+	CACTUS("cactus","Cactus"),
+//	PLUMTREE(,"Plum Tree"),
+	ROSE("rose","Red Rose"),
+	ORANGE_TREE("fruit-tree","Orange Tree"),
+	DAFFODIL("daffodil","Yellow Daffodil"),
+	GREENBEANS("beans","Green Beans"),
+	BLUEBERRY("blueberry","Blue Berries"),
+//	INDIGO(,"Indigo Flower"),
+	VIOLET("violets","Violets");
+//	RAINBOWSEED(,"RAINBOW SEED");
 
 	private String fileName, itemName;
 
