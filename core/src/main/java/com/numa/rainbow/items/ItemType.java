40a65c6c1b8b0b1859b3300393fcb6a39fbc660e
Darzington <team@darzalgames.com>
2026-05-02 14:44:11 -0400

    more icons wip

--- core/src/main/java/com/numa/rainbow/items/ItemType.java
+++ core/src/main/java/com/numa/rainbow/items/ItemType.java
@@ -3,7 +3,7 @@ package com.numa.rainbow.items;
 public enum ItemType {
 	SUMMERSEED("sesame","Seeds"),
 	DIRT("dirt","Dirt"),
-	WEED("broken-pottery","Weeds"),//FIX
+	WEED("evil-bud","Weeds"),
 	GRASS("grass","Grass"),
 	WATER("watering-can","Water"),
 	ROCK("rock","Rock"),
@@ -15,13 +15,13 @@ public enum ItemType {
 	SPRINGSEED("bulb","Seeds"),
 	WINTERSEED("ground-sprout","Seeds"),
 	BAMBOO("bamboo","Bamboo"),
-	BUSH("broken-pottery","Bush"),//FIX
+	BUSH("bush","Bush"),
 	WHEAT("wheat","Wheat"),
 	SCARECROW("scarecrow","Scarecrow"),
 	TREE("oak","Tree"),
 	AXE("axe","Axe"),
 	STICK("stick","Stick"),
-	TULIP("broken-pottery","Tulip"),//FIX
+	TULIP("tulip","Tulip"),
 	BERRYBUSH("berry-bush","Berry Bush"),
 	VINE("vines","Vines"),
 	BRAMBLES("brambles","Brambles"),
@@ -33,13 +33,13 @@ public enum ItemType {
 	HOPS("hops","Hops"),	
 	SEAWEED("seaweed","Seaweed"),
 	CACTUS("cactus","Cactus"),
-	PLUMTREE("broken-pottery","Plum Tree"),//FIX
+	PLUMTREE("fruit-tree","Plum Tree"),
 	ROSE("rose","Red Rose"),
-	ORANGE_TREE("fruit-tree","Orange Tree"),
+	ORANGE_TREE("orange-tree","Orange Tree"),
 	DAFFODIL("dafffodil","Yellow Daffodil"),
 	GREENBEANS("beans","Green Beans"),
 	BLUEBERRY("blueberry","Blue Berries"),
-	INDIGO("broken-pottery","Indigo Flower"),//FIX
+	INDIGO("indigo","Indigo Flower"),
 	VIOLET("violets","Violets"),
 	RAINBOWSEED("broken-pottery","RAINBOW SEED");//FIX
 
